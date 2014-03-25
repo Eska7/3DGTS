2d4725bcfd460d9f1f29e44a19f5a04bb8b42b03
Antoine De Gieter <antoine.degieter@me.com>
2014-03-25 11:44:22 +0100

    Sources

--- src/com/iutbm/dgts/Source.java
+++ src/com/iutbm/dgts/Source.java
@@ -13,12 +13,12 @@ public class Source {
 
     static {
         FICHIER = new HashMap<String, String>();
-        FICHIER.put("cone","http://gts.sourceforge.net/samples/cone.gts.gz");
-        FICHIER.put("bunny","http://gts.sourceforge.net/samples/bunny.gts.gz");
-        FICHIER.put("cube","http://gts.sourceforge.net/samples/cube.gts.gz");
-        FICHIER.put("epcot","http://gts.sourceforge.net/samples/epcot.gts.gz");
-        FICHIER.put("goblet","http://gts.sourceforge.net/samples/goblet.gts.gz");
-        FICHIER.put("head","http://gts.sourceforge.net/samples/head.gts.gz");
+        FICHIER.put("cone","http://ns303921.ovh.net/gts/cone.gts");
+        FICHIER.put("bunny","http://ns303921.ovh.net/gts/bunny.gts");
+        FICHIER.put("cube","http://ns303921.ovh.net/gts/cube.gts");
+        FICHIER.put("epcot","http://ns303921.ovh.net/gts/epcot.gts");
+        FICHIER.put("goblet","http://ns303921.ovh.net/gts/goblet.gts");
+        FICHIER.put("head","http://ns303921.ovh.net/gts/head.gts");
     }
 
 }
